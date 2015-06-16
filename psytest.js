80f77092e15ce4eea2d1810d764e41b24fbd4511
Paul Cuciureanu <paulcu@gmail.com>
2015-06-16 11:44:51 -0400

    Diagnostic orion for android repo, initial commit

---
+++ psytest.js
@@ -0,0 +1,37 @@
+// See: http://stackoverflow.com/questions/15959501/how-to-add-cors-headers-to-a-meteor-app/19952565#19952565
+
+// OrionJS GitHub issue: https://github.com/orionjs/orion/issues/205
+var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implementation
+
+// attach connect-style middleware for response header injection
+Meteor.startup(function () {
+  connectHandler.use(function (req, res, next) {
+    res.setHeader('Strict-Transport-Security', 'max-age=2592000; includeSubDomains'); // 2592000s / 30 days
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    return next();
+  })
+})
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
